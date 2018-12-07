120a7a80c70b4a6e679065ae91ba7eb136110486
Chinseok Lee <me@askcompany.kr>
2018-12-07 17:22:59 +0900

    post_list 뷰 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+# Class Based View -> CBV
+# post_list = ListView.as_view(model=Post)
+
+# Function Based View -> FBV
+def post_list(request):
+    qs = Post.objects.all()
+    return render(request, 'myapp/post_list.html', {
+        'post_list': qs,
+    })
--- mydiary/urls.py
+++ mydiary/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', post_list),
 ]
 
 
