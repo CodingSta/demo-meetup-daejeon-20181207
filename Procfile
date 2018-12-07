7103113bfee04457ad27bc3b38ab208240c33763
Chinseok Lee <me@askcompany.kr>
2018-12-07 17:40:53 +0900

    Heroku 배포 준비

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mydiary.wsgi --log-file -
--- mydiary/settings.py
+++ mydiary/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# heroku 세팅
+import django_heroku
+django_heroku.settings(locals())
